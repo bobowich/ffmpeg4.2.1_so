6adb42c3541f3f7297ab161b607a853c12e0c457
437042721@qq.com <437042721@qq.com>
2019-11-11 02:36:47 +0800

    added

---
+++ ffmpeg-4.2.1/build_android.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+make clean
+export NDK=/media/cy/文件/IT/ffmpeg/android-ndk-r14b
+export SYSROOT=$NDK/platforms/android-24/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+./configure --target-os=linux \
+--prefix=$PREFIX --arch=arm \
+--disable-doc \
+--enable-shared \
+--disable-static \
+--disable-yasm \
+--disable-symver \
+--enable-gpl \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-doc \
+--disable-symver \
+--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+--enable-cross-compile \
+--sysroot=$SYSROOT \
+--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+--extra-ldflags="$ADDI_LDFLAGS" \
+$ADDITIONAL_CONFIGURE_FLAG
+make clean
+make
+make install
---
+++ ffmpeg-4.2.1/libavcodec/av1_parse.d
@@ -0,0 +1,19 @@
+libavcodec/av1_parse.o: libavcodec/av1_parse.c config.h libavutil/mem.h \
+ libavutil/attributes.h libavutil/error.h libavutil/avutil.h \
+ libavutil/common.h libavutil/macros.h libavutil/version.h \
+ libavutil/avconfig.h libavutil/intmath.h libavutil/arm/intmath.h \
+ libavutil/attributes.h libavutil/mem.h libavutil/internal.h \
+ libavutil/timer.h libavutil/log.h libavutil/arm/timer.h libavutil/cpu.h \
+ libavutil/dict.h libavutil/pixfmt.h libavutil/libm.h \
+ libavutil/intfloat.h libavutil/mathematics.h libavutil/rational.h \
+ libavcodec/av1.h libavcodec/av1_parse.h libavcodec/avcodec.h \
+ libavutil/samplefmt.h libavutil/avutil.h libavutil/buffer.h \
+ libavutil/cpu.h libavutil/channel_layout.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/samplefmt.h \
+ libavutil/hwcontext.h libavutil/frame.h libavutil/log.h \
+ libavutil/pixfmt.h libavutil/rational.h libavcodec/version.h \
+ libavutil/version.h libavcodec/get_bits.h libavutil/common.h \
+ libavutil/intreadwrite.h libavutil/bswap.h libavutil/arm/bswap.h \
+ libavutil/arm/intreadwrite.h libavutil/avassert.h libavcodec/mathops.h \
+ libavutil/reverse.h libavcodec/arm/mathops.h libavcodec/vlc.h \
+ libavcodec/bytestream.h
